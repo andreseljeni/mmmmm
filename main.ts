621754300b1bb98089ac333a5375ce33f60d3fe6
andreseljeni <109925302+andreseljeni@users.noreply.github.com>
2022-07-24 20:43:13 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    rope += -0.1
+})
+input.onButtonPressed(Button.B, function () {
+    rope += -0.1
+})
+let rope = 2
+basic.forever(function () {
+    basic.clearScreen()
+    led.plot(Math.round(rope), 2)
+    if (rope < 0) {
+        basic.showString("ganador \"A\"")
+    } else if (rope < 4) {
+        basic.showString("ganador \"B\"")
+    } else {
+    	
+    }
+})
